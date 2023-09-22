5b106c4f6fe4cfe223f33199d74d5da485443725
n.korabelnikov <n.korabelnikov@isoit.ru>
2023-09-22 23:43:53 +0300

    Add project files.

---
+++ Microservices/OrderService/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+var app = builder.Build();
+app.Run();
